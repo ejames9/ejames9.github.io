a337daf327752bfc8a481a054767b1b517cb7fef
ejames9 <foster9@icloud.com>
2016-07-04 13:17:54 -0700

    .

--- src/js/ericFosterIO.js
+++ src/js/ericFosterIO.js
@@ -843,7 +843,7 @@ const ericfosterIO = (function() {
                 .top('-35px');
     }
     //If device is mobile, kill cubeFolio and show thumbNail portfolio..
-    if (winow.innerWidth < 1000) {
+    if (window.innerWidth < 1000) {
       <'#meBrand'/>
           .position('relative')
           .display('inline')
